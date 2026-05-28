46672f0dfc9d0fecacc1141b23f5bbf3008c155e
GiselaCS <giselacriollo16@gmail.com>
2026-05-28 00:27:43 -0500

    fix: stats service URL duplicada

--- src/services/statsService.js
+++ src/services/statsService.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { getToken } from './authService'
 
-//const API_URL = 'http://localhost:5000'
 const API_URL = '/api/stats'
+
 export const getStats = async () => {
-  const response = await axios.get(`${API_URL}/stats`, {
+  const response = await axios.get(API_URL, {
     headers: { Authorization: `Bearer ${getToken()}` },
   })
   return response.data
